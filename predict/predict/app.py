68119f740e0e888e09812ffceb3806e7bd5e4054
Mathieu Laversin <mathieulaversin@outlook.fr>
2023-01-06 18:31:05 +0100

    It works but not the top_k pred

--- predict/predict/app.py
+++ predict/predict/app.py
@@ -4,28 +4,22 @@ app = Flask(__name__)
 from run import TextPredictionModel
 
 
-@app.route('/')
-def home():
-    return render_template('render.html')
 
 
 # Doesn't work but it's explained in the ReadMe
-@app.route('/predict')
-def predict_html():
-    return render_template('render.html')
 
+@app.route("/", methods=['POST', 'GET'])
+def index():
 
-@app.route("/predict", methods=["GET"])
-def request_prediction():
-
-    model = TextPredictionModel.from_artefacts(
-        "train/data/artefacts/2023-01-03-12-42-59")
-    text = request.args.get('text')
-    print(text)
-    predictions = model.predict([text])
-
-    return str(predictions)
-
+    if request.method == "POST":
+        question = request.form["question"]
+        print(question)
+        path = "C:/Users/mathi/OneDrive/Bureau/poc-to-prod-capstone/train/data/artefacts/2023-01-03-12-42-59" #Advice put your absolute path :)
+        prediction_object = TextPredictionModel.from_artefacts(path)
+        predic = prediction_object.predict([question], top_k=1)
+        print("The prediction without the top_k", predic)
+        return (str(predic))
+    return render_template("render.html")
 
 if __name__ == '__main__':
     app.run()
